10536dd6b91899fb47c3ef9c4604c2ecc4a41fbd
ZhivkoTringov <zhivkotringov@gmail.com>
2024-02-22 02:01:36 +0200

    Conf settings.py for db

--- logbook/logbook/settings.py
+++ logbook/logbook/settings.py
@@ -78,8 +78,12 @@ WSGI_APPLICATION = 'logbook.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': os.getenv("POSTGRES_ENGINE", 'django.db.backends.sqlite3'),
+        'NAME': os.getenv("POSTGRES_NAME", BASE_DIR / 'db.sqlite3'),
+        'USER': os.getenv("POSTGRES_USER", "user"),
+        "PASSWORD": os.getenv("POSTGRES_PASSWORD", "password"),
+        "HOST": os.getenv("POSTGRES_HOST", "localhost"),
+        "PORT": os.getenv("POSTGRES_PORT", "5432"),
     }
 }
 
