45a0dc20e9b4347fa1741b36e7e160b21c16a403
ZhivkoTringov <zhivkotringov@gmail.com>
2024-02-23 20:26:22 +0200

    Modify accounts/view.py

--- logbook/accounts/views.py
+++ logbook/accounts/views.py
@@ -1,22 +1,16 @@
 from django.contrib.auth import get_user_model
 from django.shortcuts import render
 from rest_framework.authtoken import views as api_auth_views
-from rest_framework import generics as api_views, serializers
+from rest_framework import generics as api_views
+
+from accounts.serializers import RegisterUserSerializer
 
 UserModel = get_user_model()
 
 class ApiLoginUserView(api_auth_views.ObtainAuthToken):
     pass
 
-class RegisterUserSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = UserModel
-        fields = (UserModel.USERNAME_FIELD, 'password')
 
-    def to_representation(self, instance):
-        data = super().to_representation(instance)
-        data.pop('password')
-        return data
 
 class ApiRegisterUserView(api_views.CreateAPIView):
     serializer_class = RegisterUserSerializer
