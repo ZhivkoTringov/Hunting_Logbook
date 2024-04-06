ed8a4e0b67ce9e639c112fb17ca6d03f1edb6b1a
ZhivkoTringov <zhivkotringov@gmail.com>
2024-04-06 03:05:04 +0300

    Add Navigation

--- frontend/src/components/navbar/Navbar.jsx
+++ frontend/src/components/navbar/Navbar.jsx
@@ -1,9 +1,38 @@
 import React from 'react';
+import { RiMenu3Line, RiCloseLin } from 'react-icons/ri'
+import logo from '../../assets/Hunting.png'
 import './navbar.css';
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <div className='hl__navbar'>
+      <div className='hl__navbar-links'>
+        <div className='hl__navbar-links_logo'>
+          <img src={logo} alt='logo' />
+        </div>
+        <div className='hl__navbar-links_container'>
+          <p>
+            <a href="#home">Home</a>
+          </p>
+          <p>
+            <a href="#home">Clubs</a>
+          </p>
+          <p>
+            <a href="#home">Individuals</a>
+          </p>
+          <p>
+            <a href="#home">News</a>
+          </p>
+          <p>
+            <a href="#home">About</a>
+          </p>
+        </div>
+      </div>
+      <div className='hl__navbar-sign'>
+        <p>Sign in</p>
+        <button type='button'>Sign up</button>
+      </div>
+    </div>
   )
 }
 
